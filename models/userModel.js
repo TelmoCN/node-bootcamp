5ce4dac78852bce1ff4339037187cf0213729a9f
Telmo Sampaio <telmo.sampaio@wearecodenation.com>
2020-02-26 13:29:49 +0000

    Creating new users

--- models/userModel.js
+++ models/userModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
-const tourSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, "Please tell us your name"]
